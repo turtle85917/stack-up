7b5f51e69131c287663e3613b391d550ee684134
Pulotodom <61219276+turtle85917@users.noreply.github.com>
2024-08-17 15:14:04 +0900

    🔨 feat: Setting global variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-import './style.css';
+import "./style.css";
+
+const gameCanvas = document.querySelector<HTMLCanvasElement>("canvas.game");
+if(gameCanvas === null)
+  throw new Error("Not found 'game' canvas!");
+const ctx = gameCanvas.getContext("2d");
+if(ctx === null)
+  throw new Error("Not found context in game canvas...");
+
+$game = gameCanvas;
